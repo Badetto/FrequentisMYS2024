a98e9a5d86faa3035fc19151b51817f754420d3c
BADU Catalin-Ioachim <BADU.Catalin-Ioachim@frequentis.com>
2024-08-06 16:12:03 +0300

    Feature 1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -10,6 +13,11 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+            i = i + 8;
+            List<Integer> myList = new ArrayList<>();
+            for (int j = 1; j<=i; j++){
+                myList.add(j);
+            }
         }
     }
 }
